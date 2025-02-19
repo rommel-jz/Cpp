5f280d320fd74b138c6132990b9af1e8fe1aea12
rommel-jz <1322701698@qq.com>
2025-02-19 20:30:40 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
-
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int BinarySearch(int arr[], int left, int right, int target) {
+	
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+		if (arr[mid] == target) {
+			return mid;
+		}
+		else if (arr[mid] > target) {
+			right = mid - 1;
+		}
+		else {
+			left = mid + 1;
+		}
+	}
+	return -1;
+}
+int main() {
+	int num[] = { 13,45,2,433,54,23,64,1,3,6,7 };
+	sort(num, num+11);
+	cout << BinarySearch(num, 0, 10, 7);
+	return 0;
+}
